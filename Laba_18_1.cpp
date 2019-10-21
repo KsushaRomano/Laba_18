c10a33a241533faee39872e0ea47a24152f73937
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 12:53:00 +0300

    Update Laba_18_1.cpp

--- Laba_18_1.cpp
+++ Laba_18_1.cpp
@@ -9,33 +9,38 @@ int main()
 	int M, i, j, k, m, c;
 	cout << "Введите размер квадратного массива: ";
 	cin >> M;
-	float** A = new float* [M];
-	for (c = 0; c < M; c++)
-		A[c] = new float[M];
-	cout << "Введите элементы массива: " << endl;
-	for (i = 0; i < M; i++) {
-		for (j = 0; j < M; j++) {
-			cin >> A[i][j];
+	if ((M % 10 == 1) || (M % 10 == 1) || (M % 10 == 1) || (M % 10 == 1) || (M % 10 == 1)){
+		float** A = new float* [M];
+		for (c = 0; c < M; c++)
+			A[c] = new float[M];
+		cout << "Введите элементы массива: " << endl;
+		for (i = 0; i < M; i++) {
+			for (j = 0; j < M; j++) {
+				cin >> A[i][j];
+			};
 		};
+		cout << "Вывод массива змейкой: " << endl;
+		for (m = 0; m < M / 2; m++) {
+			for (i = m; i < M - m; i++) {
+				cout << A[i][m];
+				cout << endl;
+			};
+			for (j = m + 1; j <= M - m - 1; j++) {
+				cout << A[M - m -1][j];
+				cout << endl;
+			};
+			for (i = M - m - 2; i >= m; i--) {
+				cout << A[i][M - m - 1];
+				cout << endl;
+			};
+			for (j = M - m - 2; j >= m + 1; j--) {
+				cout << A[m][j];
+				cout << endl;
+			};
+		};
+		cout << A[M / 2][M / 2];
+	}
+	else {
+	cout << "Введен неверный размер матрицы. М должно быть нечетным";
 	};
-	cout << "Вывод массива змейкой: " << endl;
-	for (m = 0; m < M / 2; m++) {
-		for (i = m; i < M - m; i++) {
-			cout << A[i][m];
-			cout << endl;
-		};
-		for (j = m + 1; j <= M - m - 1; j++) {
-			cout << A[M - m -1][j];
-			cout << endl;
-		};
-		for (i = M - m - 2; i >= m; i--) {
-			cout << A[i][M - m - 1];
-			cout << endl;
-		};
-		for (j = M - m - 2; j >= m + 1; j--) {
-			cout << A[m][j];
-			cout << endl;
-		};
-	};
-	cout << A[M / 2][M / 2];
 };
